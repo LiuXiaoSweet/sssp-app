17614fc32fcb6969a9af35d2b6d3c610ba988bf2
张乐平 <holynull@126.com>
2020-09-27 13:31:07 +0800

    lala

--- src/app/swap-comp/swap-comp.component.ts
+++ src/app/swap-comp/swap-comp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import BigNumber from 'bignumber.js';
+import { BootService } from '../services/boot.service';
 
 @Component({
     selector: 'app-swap-comp',
@@ -8,16 +10,50 @@ import { Component, OnInit } from '@angular/core';
 export class SwapCompComponent implements OnInit {
     left: string = '0';
 
-    right: string = '0';
+    right: string = '1';
 
-    constructor() { }
+    balance: BigNumber;
+
+    amt: string;
+
+    constructor(public boot: BootService) { }
 
     ngOnInit(): void {
     }
     chooseLeft(val) {
-        console.log('left ' + val);
+        this.left = val;
+        if (this.left === this.right) {
+            if (this.right !== '2') {
+                this.right = String(Number(this.right) + 1);
+            } else {
+                this.right = '0';
+            }
+        }
+        console.log('left ' + this.left);
     }
     chooseRight(val) {
-        console.log('right ' + val);
+        this.right = val;
+        if (this.left === this.right) {
+            if (this.left !== '2') {
+                this.left = String(Number(this.right) + 1);
+            } else {
+                this.left = '0';
+            }
+        }
+        console.log('right ' + this.right);
+    }
+
+    maxAmt() {
+        switch (this.left) {
+            case '0':
+                this.amt = this.boot.balance.dai.toFixed(9);
+                break;
+            case '1':
+                this.amt = this.boot.balance.busd.toFixed(9);
+                break;
+            case '2':
+                this.amt = this.boot.balance.usdt.toFixed(9);
+                break;
+        }
     }
 }
